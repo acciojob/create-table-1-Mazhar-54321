60232601e81fed13e76dde0999e8468607b5f458
Mazhar-54321 <160312563+Mazhar-54321@users.noreply.github.com>
2025-05-09 08:29:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+  //Write your code here
+ const table = document.getElementById("sampleTable");
+ const children = table.childNodes;
+ 
+ console.log(children);
+ const tr = document.createElement("tr");
+ const cell1 = document.createElement("td");
+ cell1.textContent="New Cell1"
+ const cell2 = document.createElement("td");
+ cell2.textContent="New Cell2";
+ tr.appendChild(cell1);
+ tr.appendChild(cell2);
+ table.prepend(tr);
+// table.appendChild(children)
+
 }
